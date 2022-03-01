144e66d9a5a4917e2712aedfc15ae051185c74d5
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2022-03-01 09:41:57 +0300

    .

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,78 @@
+<?php
+
+if (!function_exists('isSynced')) {
+    function isSynced(array $result): bool
+    {
+        return count($result['attached']) > 0 || count($result['updated']) > 0;
+    }
+}
+
+if (!function_exists('isUnsynced')) {
+    function isUnsynced(array $result): bool
+    {
+        return count($result['detached']) > 0;
+    }
+}
+
+if (!function_exists('dateToShow')) {
+    function dateToShow(string $date): string
+    {
+        return \Carbon\Carbon::make($date)->format('d.m H:i');
+    }
+}
+
+if (!function_exists('renderPrice')) {
+    function renderPrice(?int $price)
+    {
+        return number_format($price/100, 2, '.', ' ') . ' BYN';
+    }
+}
+
+if (!function_exists('renderPhone')) {
+    function renderPhone(string $phone): string
+    {
+        return $phone;
+        $result = '';
+        $phone = str_split(strrev($phone));
+
+        dd(array_slice($phone, 2), $phone);
+        // return
+    }
+}
+
+if (!function_exists('getCookie')) {
+    function getCookie(string $key): ?string
+    {
+        return \Illuminate\Support\Facades\Cookie::get($key);
+    }
+}
+
+if (!function_exists('setCookie')) {
+    function setCookie(string $key, $value)
+    {
+        return \Illuminate\Support\Facades\Cookie::queue(\Illuminate\Support\Facades\Cookie::forever($key, $value));
+    }
+}
+
+if (!function_exists('str')) {
+    function str(string $string): \Illuminate\Support\Stringable
+    {
+        return \Illuminate\Support\Str::of($string);
+    }
+}
+
+// Bindings
+
+if (!function_exists('cartRepository')) {
+    function cartRepository()
+    {
+        return app(\App\Repositories\CartRepositoryInterface::class);
+    }
+}
+
+if (!function_exists('cartService')) {
+    function cartService()
+    {
+        return app(\App\Services\CartServiceInterface::class);
+    }
+}
